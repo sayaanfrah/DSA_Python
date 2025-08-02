ffab52f2682876ef4c041d9c987a3b91314dd513
sayaanfrah <sayaanfrah@gmail.com>
2025-08-02 12:14:44 +0530

    update

---
+++ first/newo.java
@@ -0,0 +1,9 @@
+class newo{
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int a=23;
+        int b=234;
+        int c=(a*b);
+        System.out.println("c="+c);
+    }
+}
